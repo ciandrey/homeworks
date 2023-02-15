b8a3524320c1e72083af2a29d1dc7485ba4aa771
ciandrey <ciandrey@tut.by>
2023-02-15 11:23:46 +0300

    Домашнее задание-Занятие 8

--- lesson-08/8.js
+++ lesson-08/8.js
@@ -1,21 +1,21 @@
 // 1. поменять массив в обратном порядке - [1,2,3,4,5,6] [6,5,4,3,2,1]
-var myArray = [1,2,3,4,5,6];
+let myArray = [1, 2, 3, 4, 5, 6];
 myArray.reverse();
 
 // 2. найти максимальное значение числа в массиве ([3,67,15...])
-let arr = [3,67,15,58,123654,0,-5];
-Math.max(...arr);
+let myArr = [3,67,15,58,123654,0,-5];
+Math.max(...myArr);
 
 // 3. записать в массив ряд фибоначчи начиная с N члена с длинной массива M
-let N = 0;
-let M = 10;
+let numN = 0;
+let dimM = 10;
 
 let fibonacci = [0, 1];
 
-for (i = 2; i < M+N; i++) {
+for (i = 2; i < dimM+numN; i++) {
     fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
 }
-fibonacci.slice(N);
+fibonacci.slice(numN);
 
 // 4. даны 2 4-х значных числа с неповторяющимися цифрами, надо определить сколько цифр в этих числах совпадают по значению и позиции
 // и сколько только по значению (3487 и 3794 ---> 1 и 2 )
@@ -28,10 +28,14 @@ let
     arr2 = ('' + num2).split('');
 for(let i=0; i<arr1.length; ++i) {
     for(let j=0; j<arr2.length; ++j) {
-        if(arr1[i] === arr2[j] && i===j)
+        if(arr1[i] === arr2[j] && i===j){
             ++count1;
-        if(arr1[i] === arr2[j] && i!==j)
+        }
+
+        if(arr1[i] === arr2[j] && i!==j){
             ++count2;
+        }
+
     }
 }
 
@@ -45,6 +49,6 @@ list.sort();
 list.reverse();
 
 // 6. удалить из массива все повторяющиеся элементы
-let x = [55, 44, 55, 30, 30, 'i', 'kk', 'i', 'kk', 0, -1, -1, 0];
-let unique = x.filter((e, i) => x.indexOf(e) === i );
-let unirr = [...new Set(x)];
+let someThing = [55, 44, 55, 30, 30, 'i', 'kk', 'i', 'kk', 0, -1, -1, 0];
+let unique = someThing.filter((e, i) => someThing.indexOf(e) === i );
+let unirr = [...new Set(someThing)];
